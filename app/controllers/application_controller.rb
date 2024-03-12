3441fd8594aeb7ab35f475b30ab60c27a11efe27
manishaparajuli <manisaparajuli66@gmail.com>
2024-03-12 16:32:18 +1100

    signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user_or_restaurant, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_no])
   end
+
+
+  def authenticate_user_or_restaurant
+    unless user_signed_in? || restaurant_signed_in?
+      redirect_to root_path, alert: 'You must be signed in.'
+    end
+  end
+
 end
