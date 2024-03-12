c459b8fffa0628151c6521ad1ab8d856c081401c
Debbie Kang <dhyk82@gmail.com>
2024-03-12 20:44:47 +1100

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_no])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_no, :name, :cuisine, :phone_no, :rating, :email, :address, :pickup_start, :pickup_end ])
   end
 
 
