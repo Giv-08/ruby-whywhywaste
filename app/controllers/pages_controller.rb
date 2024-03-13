d0d6744309dcbe9cadac72d78f55b194c275a0a2
chutima puthachon <chutima.p8@gmail.com>
2024-03-13 11:50:07 +1100

    progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user_or_restaurant, only: [ :home ]
 
   def dashboard
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @food = Food.find(params[:food_id])
+  end
+
+  def show
+    @restaurants = Restaurant.all
+    @foods = Food.all
   end
 end
