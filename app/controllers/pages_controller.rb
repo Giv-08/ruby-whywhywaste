8aa101a8c2c166efbc813d5ea35f5b660dc3ea48
manishaparajuli <manisaparajuli66@gmail.com>
2024-03-13 11:36:37 +1100

    created link between root page and different authentication pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user_or_restaurant, only: [ :home ]
+  skip_before_action :authenticate_user_or_restaurant, only: [ :home, :authentication ]
 
   def home
+  end
+
+  def authentication
+    # sign_in for both buttons
+    # sign_up for both buttons
+    render layout: 'no_navbar'
   end
 
 end
