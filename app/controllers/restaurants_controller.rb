2c102abf13deb32c336a1242c565395af28487b7
Debbie Kang <dhyk82@gmail.com>
2024-03-14 16:34:14 +1100

    updates on restaurants user index and restaurants  show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@ class RestaurantsController < ApplicationController
   def index
 
     @restaurants = Restaurant.all
-  
+
 
     # Signed-in restaurant only
     if restaurant_signed_in?
@@ -14,35 +14,11 @@ class RestaurantsController < ApplicationController
       # fetches all restaurants
       @my_restaurant = nil
       @other_restaurants = Restaurant.all
-
-      render 'restaurant_card', restaurant: @my_restaurant
     end
 
     # Grab all foods from signed-in restaurant
     @foods = Food.all
   end
-
-
-  # def index
-  #   if current_user&.restaurant?
-  #     # Signed-in restaurant only
-  #     if restaurant_signed_in?
-  #       @my_restaurant = current_restaurant
-
-  #       # fetch all other restaurants (excluding signed-in restaurant)
-  #       @other_restaurants = Restaurant.where.not(id: @my_restaurant&.id)
-  #       render 'restaurants/index_for_restaurant'
-  #     else
-  #       # fetches all restaurants
-  #       @my_restaurant = nil
-  #       @other_restaurants = Restaurant.all
-  #       render 'restaurants/index_for_restaurant'
-  #     end
-  #   else
-  #     render 'restaurants/index_for_customer'
-
-
-
 
   def show
     @restaurant = Restaurant.find(params[:id])
