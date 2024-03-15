f73d0c83e24a64f9adf7e9bd09288d867511bcfd
manishaparajuli <manisaparajuli66@gmail.com>
2024-03-15 11:53:36 +1100

    created cart

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   get 'users/dashboard', to: 'users/dashboard#dashboard'
   resources :restaurants do
     # Nested routes - foods belonging to restaurant
-    resources :foods, only: [:new, :create]
+    resources :foods, only: [:new, :create, :edit]
     # Nested routes - reviews belonging to restaurant
     resources :reviews, only: [:index, :create, :new]
     # Nested routes - orders related to a specific restaurant
@@ -45,6 +45,13 @@ Rails.application.routes.draw do
   get '/search/map', to: 'search#map', as: :search_map
 
   get '/cart', to: 'cart#cart', as: :cart
+  get 'order_lines/:id' => "order_lines#show", as: "order_lines"
+  get 'order_lines/:id/add' => "order_lines#add_quantity", as: "orders"
+  post 'order_lines/:id/add' => "order_lines#add_quantity", as: "order_lines_add"
+  get 'order_lines/:id/reduce' => "order_lines#reduce_quantity", as: "orders_reduce"
+  post 'order_lines/:id/reduce' => "order_lines#reduce_quantity", as: "order_lines_reduce"
+
+  delete 'order_lines/:id' => "order_lines#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
