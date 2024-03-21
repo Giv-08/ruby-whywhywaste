02aac456814699c005b5f0eb26a676d19eefe8f3
chutima puthachon <chutima.p8@gmail.com>
2024-03-21 15:56:23 +1100

    stopped cart items from moving

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,5 +3,7 @@ class CartController < ApplicationController
 
   def cart
     @order = Order.find_or_create_by(user: current_user, status: :cart)
+    @order_sorted = @order.order_lines.order(created_at: :desc)
+    @order_sorted
   end
 end
